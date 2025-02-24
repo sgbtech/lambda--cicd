45e4cdf34d36587db5b2e4f268f61f60ae2a88fe
sgbtech <sgbtech62@gmail.com>
2025-02-24 19:24:40 +0000

    9th update, this time to lambda-function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from CICD lambda')
+    'body': json.dumps('Hello from CICD lambda, today is the 24.02.2025')
     }
